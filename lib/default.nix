b0a904535a9f69c9166b6966a9347ae7ab2512ef
LukeTandjung <lukelucus123@gmail.com>
2025-10-19 19:49:11 +0100

    Fixed?

--- lib/default.nix
+++ lib/default.nix
@@ -2,6 +2,7 @@ inputs: self: super: {
   nixosSystem' = config: {
     class = "nixos";
     config = super.nixosSystem {
+      system = "x86_64-linux";
       specialArgs = { 
         inherit inputs;
         lib = self;
@@ -13,6 +14,7 @@ inputs: self: super: {
   darwinSystem' = config: {
     class = "darwin";
     config = inputs.nix-darwin.lib.darwinSystem {
+      system = "aarch64-darwin";
       specialArgs = { 
         inherit inputs;
         lib = self;
