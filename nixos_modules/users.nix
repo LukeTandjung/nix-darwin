b7af6c5528668491e922bd5dc5130c9d3abe3d80
LukeTandjung <lukelucus123@gmail.com>
2025-10-22 10:31:48 +0100

    Added zsh

--- nixos_modules/users.nix
+++ nixos_modules/users.nix
@@ -8,6 +8,10 @@
     shell = pkgs.zsh;
   };
 
+  # Even though this option is enabled by HM, this only enables zsh for the user level;
+  # we need to enable zsh systemwide. Otherwise, zsh might miss systemwide nix directories.
+  programs.zsh.enable = true;
+
   # Sudo without password for wheel users
   security.sudo = {
     enable = true;
