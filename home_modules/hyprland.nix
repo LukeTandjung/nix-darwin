dff0e3adffea00dd814c0c816df9f40dc42713b1
LukeTandjung <lukelucus123@gmail.com>
2025-10-22 18:49:12 +0100

    removed conflicting color definitions

--- home_modules/hyprland.nix
+++ home_modules/hyprland.nix
@@ -34,8 +34,6 @@ lib.mkIf pkgs.stdenv.isLinux {
         gaps_in = 5;
         gaps_out = 20;
         border_size = 2;
-        "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
-        "col.inactive_border" = "rgba(595959aa)";
         resize_on_border = false;
         allow_tearing = false;
         layout = "dwindle";
@@ -50,7 +48,6 @@ lib.mkIf pkgs.stdenv.isLinux {
           enabled = true;
           range = 4;
           render_power = 3;
-          color = "rgba(1a1a1aee)";
         };
 
         blur = {
