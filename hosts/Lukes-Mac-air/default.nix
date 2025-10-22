a64adc00fdea8a9f87854ecf97f359aed0ae135a
LukeTandjung <lukelucus123@gmail.com>
2025-10-22 13:16:18 +0100

    What is this?

--- hosts/Lukes-Mac-air/default.nix
+++ hosts/Lukes-Mac-air/default.nix
@@ -1,10 +1,6 @@
 { inputs, ... }: let
   inherit (inputs) nixpkgs home-manager stylix zen-browser spicetify-nix dankMaterialShell;
   sharedHomeModules = [
-    # While importing Stylix as a NixOS module also does so for HM, rebuilding without explicitly
-    # importing the HM module causes build errors. Apparently this is because it is not imported
-    # into HM user subtree at runtime.
-    stylix.homeModules.stylix
     zen-browser.homeModules.beta
     spicetify-nix.homeManagerModules.spicetify
     dankMaterialShell.homeModules.dankMaterialShell.default
