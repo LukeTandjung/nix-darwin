9731c4e0c6170b29448c1b5de0f993bd8ed4d12d
LukeTandjung <lukelucus123@gmail.com>
2025-11-26 23:12:21 +0000

    Increased fastfetch size

--- home_modules/fastfetch.nix
+++ home_modules/fastfetch.nix
@@ -15,8 +15,8 @@
       logo = {
         source = "./assets/hokusai.png";
         type = "kitty-direct";
-        width = 10;
-        height = 10;
+        width = 100;
+        height = 100;
         padding = {
           top = 1;
           left = 3;
