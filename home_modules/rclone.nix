442cb6d0251bb5e89a7614a656622c92a9924b16
LukeTandjung <lukelucus123@gmail.com>
2025-11-28 16:28:39 +0000

    Added rclone settings for caching and faster image prefetching

--- home_modules/rclone.nix
+++ home_modules/rclone.nix
@@ -37,9 +37,15 @@
           enable = true;
           mountPoint = "${config.home.homeDirectory}/Pictures";
           options = {
-            vfs-cache-mode = "writes";
-            dir-cache-time = "12h";
-            poll-interval = "30s";
+            vfs-cache-mode = "full";
+            vfs-cache-max-size = "1G";
+            vfs-cache-max-age = "168h";
+            vfs-read-chunk-size = "16M";
+            vfs-read-chunk-size-limit = "64M";
+            vfs-read-ahead = "64M";
+            dir-cache-time = "24h";
+            attr-timeout = "24h";
+            poll-interval = "1m";
             cache-dir = "${config.home.homeDirectory}/.cache/rclone";
             umask = "0022";
           };
