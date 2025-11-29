94dda31e910af28d0a5aff69bad83b4b0730ebcd
LukeTandjung <lukelucus123@gmail.com>
2025-11-29 12:43:48 +0000

    Added setting import

--- home_modules/helix.nix
+++ home_modules/helix.nix
@@ -21,20 +21,22 @@
         formatter.command = "typstyle";
       }
     ];
-    keys.normal = {
-      C.y = [
-        ":sh rm -f /tmp/unique-file"
-        ":insert-output yazi %{buffer_name} --chooser-file=/tmp/unique-file"
-        ":insert-output echo '\x1b[?1049h\x1b[?2004h' > /dev/tty"
-        ":open %sh{sudo cat /tmp/unique-file}"
-        ":redraw"
-      ];
-      C.g = [
-        ":new"
-        ":insert-output lazygit"
-        ":buffer-close!"
-        ":redraw"
-      ];
+    settings = {
+      keys.normal = {
+        C.y = [
+          ":sh rm -f /tmp/unique-file"
+          ":insert-output yazi %{buffer_name} --chooser-file=/tmp/unique-file"
+          ":insert-output echo '\x1b[?1049h\x1b[?2004h' > /dev/tty"
+          ":open %sh{sudo cat /tmp/unique-file}"
+          ":redraw"
+        ];
+        C.g = [
+          ":new"
+          ":insert-output lazygit"
+          ":buffer-close!"
+          ":redraw"
+        ];
+      };
     };
   };
 }
