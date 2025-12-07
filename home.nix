511a2e33e00b302c2b232a5645fea8525c09f774
LukeTandjung <lukelucus123@gmail.com>
2025-12-07 20:46:08 +0000

    Added helix to default editor home environment

--- home.nix
+++ home.nix
@@ -7,18 +7,25 @@
 }:
 let
   isDarwin = pkgs.stdenv.isDarwin;
-  isLinux = pkgs.stdenv.isLinux;
 in
 {
-  home.username = if isDarwin then "luketandjung" else "luke";
-  home.homeDirectory = if isDarwin then "/Users/luketandjung" else "/home/luke";
-  home.stateVersion = "25.05";
   programs.home-manager.enable = true;
+
+  home = {
+    username = if isDarwin then "luketandjung" else "luke";
+    homeDirectory = if isDarwin then "/Users/luketandjung" else "/home/luke";
+    stateVersion = "25.05";
+    packages = [ ];
+
+    # This suppresses the login message that appears for Kitty!
+    file.".hushlogin".text = "";
+
+    sessionVariables = {
+      EDITOR = "hx";
+    };
+  };
+
   imports = [
     ./home_modules
   ];
-  home.packages = [ ];
-
-  # This suppresses the login message that appears for Kitty!
-  home.file.".hushlogin".text = "";
 }
