7224301d6c925e56d1088047266bd54798d3ec43
LukeTandjung <lukelucus123@gmail.com>
2026-01-21 16:43:40 +0000

    Removed overlay

--- hosts/Lukes-Mac-mini/configuration.nix
+++ hosts/Lukes-Mac-mini/configuration.nix
@@ -38,20 +38,20 @@
   nixpkgs = {
     hostPlatform = "aarch64-darwin";
     config.allowUnfree = true;
-    overlays = [
-      (final: prev: {
-        spotify = prev.spotify.overrideAttrs (oldAttrs: {
-          src =
-            if (prev.stdenv.isDarwin && prev.stdenv.isAarch64) then
-              prev.fetchurl {
-                url = "https://web.archive.org/web/20251029235406/https://download.scdn.co/SpotifyARM64.dmg";
-                hash = "sha256-0gwoptqLBJBM0qJQ+dGAZdCD6WXzDJEs0BfOxz7f2nQ=";
-              }
-            else
-              oldAttrs.src;
-        });
-      })
-    ];
+    # overlays = [
+    #   (final: prev: {
+    #     spotify = prev.spotify.overrideAttrs (oldAttrs: {
+    #       src =
+    #         if (prev.stdenv.isDarwin && prev.stdenv.isAarch64) then
+    #           prev.fetchurl {
+    #             url = "https://web.archive.org/web/20251029235406/https://download.scdn.co/SpotifyARM64.dmg";
+    #             hash = "sha256-0gwoptqLBJBM0qJQ+dGAZdCD6WXzDJEs0BfOxz7f2nQ=";
+    #           }
+    #         else
+    #           oldAttrs.src;
+    #     });
+    #   })
+    # ];
   };
 
   fonts.packages = with pkgs; [
