a9e70ec37065dc27c735e22e46f1c9fe161b3ece
LukeTandjung <lukelucus123@gmail.com>
2026-02-06 16:04:39 +0000

    Added configurational options for DMS

--- home_modules/dank-material-shell.nix
+++ home_modules/dank-material-shell.nix
@@ -8,6 +8,18 @@
   programs.dank-material-shell = {
     enable = true;
     enableCalendarEvents = false;
+    settings = {
+      fontFamily = "JetBrains Mono";
+      cornerRadius = 4;
+      showWorkspaceApps = true;
+      showClipboard = false;
+      launcherLogoMode = "os";
+      launcherLogoColorOverride = "primary";
+    };
+    session = {
+      weatherLocation = "London, UK";
+      weatherCoordinates = "51.5074,-0.1278";
+    };
     plugins.dms-power-usage = {
       src = pkgs.fetchFromGitHub {
         owner = "Daniel-42-z";
