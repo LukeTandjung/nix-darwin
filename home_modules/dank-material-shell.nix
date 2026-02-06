50d197d701342a077ea6bd8e8d9233177d24d760
LukeTandjung <lukelucus123@gmail.com>
2026-02-06 15:42:59 +0000

    Added correct hash

--- home_modules/dank-material-shell.nix
+++ home_modules/dank-material-shell.nix
@@ -13,7 +13,7 @@
         owner = "Daniel-42-z";
         repo = "dms-power-usage";
         rev = "main";
-        hash = "";
+        hash = "sha256-9lMWVCiAW+0fAZD4dXGhE+6f8d1/4DHejfUumfoHY2s=";
       };
     };
   };
