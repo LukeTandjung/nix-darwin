eec4f04a943aab7dc111830cf3a68f24e2d325b9
LukeTandjung <lukelucus123@gmail.com>
2026-02-06 23:27:57 +0000

    moved settings to session

--- home_modules/dank-material-shell.nix
+++ home_modules/dank-material-shell.nix
@@ -18,6 +18,57 @@ lib.mkIf pkgs.stdenv.isLinux {
         launcherLogoMode = "os";
         launcherLogoColorOverride = "primary";
         wallpaperPath = "/home/luke/Pictures/Wallpapers/painting_in_balcony.jpeg";
+        barConfigs = [
+          {
+            id = "default";
+            name = "Main Bar";
+            enabled = true;
+            position = 0;
+            screenPreferences = [ "all" ];
+            showOnLastDisplay = true;
+            leftWidgets = [
+              "launcherButton"
+              "workspaceSwitcher"
+              "focusedWindow"
+            ];
+            centerWidgets = [
+              "music"
+              "clock"
+              "weather"
+            ];
+            rightWidgets = [
+              "systemTray"
+              "cpuUsage"
+              "memUsage"
+              "notificationButton"
+              "battery"
+              "controlCenterButton"
+            ];
+            spacing = 4;
+            innerPadding = 4;
+            bottomGap = 0;
+            transparency = 1;
+            widgetTransparency = 1;
+            squareCorners = false;
+            noBackground = false;
+            gothCornersEnabled = false;
+            gothCornerRadiusOverride = false;
+            gothCornerRadiusValue = 12;
+            borderEnabled = false;
+            borderColor = "surfaceText";
+            borderOpacity = 1;
+            borderThickness = 1;
+            fontScale = 1;
+            autoHide = false;
+            autoHideDelay = 250;
+            openOnOverview = false;
+            visible = true;
+            popupGapsAuto = true;
+            popupGapsManual = 4;
+          }
+        ];
+      };
+      session = {
         isLightMode = false;
         doNotDisturb = false;
         perMonitorWallpaper = false;
@@ -87,59 +138,6 @@ lib.mkIf pkgs.stdenv.isLinux {
         searchAppActions = true;
         vpnLastConnected = "";
         configVersion = 3;
-        barConfigs = [
-          {
-            id = "default";
-            name = "Main Bar";
-            enabled = true;
-            position = 0;
-            screenPreferences = [ "all" ];
-            showOnLastDisplay = true;
-            leftWidgets = [
-              "launcherButton"
-              "workspaceSwitcher"
-              "focusedWindow"
-            ];
-            centerWidgets = [
-              "music"
-              "clock"
-              "weather"
-            ];
-            rightWidgets = [
-              "systemTray"
-              "cpuUsage"
-              "memUsage"
-              "notificationButton"
-              "battery"
-              "controlCenterButton"
-            ];
-            spacing = 4;
-            innerPadding = 4;
-            bottomGap = 0;
-            transparency = 1;
-            widgetTransparency = 1;
-            squareCorners = false;
-            noBackground = false;
-            gothCornersEnabled = false;
-            gothCornerRadiusOverride = false;
-            gothCornerRadiusValue = 12;
-            borderEnabled = false;
-            borderColor = "surfaceText";
-            borderOpacity = 1;
-            borderThickness = 1;
-            fontScale = 1;
-            autoHide = false;
-            autoHideDelay = 250;
-            openOnOverview = false;
-            visible = true;
-            popupGapsAuto = true;
-            popupGapsManual = 4;
-          }
-        ];
-      };
-      session = {
-        weatherLocation = "London, UK";
-        weatherCoordinates = "51.5074,-0.1278";
       };
       plugins.dms-power-usage = {
         src = pkgs.fetchFromGitHub {
