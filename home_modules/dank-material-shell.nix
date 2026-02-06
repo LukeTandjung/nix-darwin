88d0754dd24782a9affd3c9820fb8d0d5fd2b09e
LukeTandjung <lukelucus123@gmail.com>
2026-02-06 15:41:43 +0000

    Managed plugins through dms nix flake

--- home_modules/dank-material-shell.nix
+++ home_modules/dank-material-shell.nix
@@ -8,5 +8,13 @@
   programs.dank-material-shell = {
     enable = true;
     enableCalendarEvents = false;
+    plugins.dms-power-usage = {
+      src = pkgs.fetchFromGitHub {
+        owner = "Daniel-42-z";
+        repo = "dms-power-usage";
+        rev = "main";
+        hash = "";
+      };
+    };
   };
 }
