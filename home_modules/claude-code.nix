8deefcbe5b6556363f8cf8d535f8633b45e85250
LukeTandjung <lukelucus123@gmail.com>
2026-02-23 02:23:33 +0000

    Added correct hash and fixed bad compilation on Linux

--- home_modules/claude-code.nix
+++ home_modules/claude-code.nix
@@ -12,7 +12,7 @@ let
 
   hashes = {
     "aarch64-darwin" = "sha256-ivshwgonybQq+nPP0x4kfpM4GKG9wEsg3Us3UBu2Hjw=";
-    "x86_64-linux" = "sha256-gT6CLl+BPyJZPs0daQippMEDV/gSaXEVqAzzQ0m9awo=";
+    "x86_64-linux" = "sha256-C3aBkOI4TVvihB2DJ6j+OUrLiyZ7E/Fr6ki7QNI2Fyc=";
   };
 in
 {
@@ -22,8 +22,7 @@ in
       pname = "claude-code-compiled";
       inherit version;
       dontUnpack = true;
-      dontPatchELF = true;
-      dontStrip = true;
+      dontFixup = true;
       nativeBuildInputs = [
         pkgs.deno
         pkgs.cacert
