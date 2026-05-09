3efcdf4e20c6acbec79992689a5b6f9e75eba6e8
LukeTandjung <lukelucus123@gmail.com>
2026-05-09 14:46:43 +0100

    Fixed node not being on PATH

--- home_modules/pi.nix
+++ home_modules/pi.nix
@@ -13,6 +13,11 @@
       defaultModel = "gpt-5.4";
       defaultThinkingLevel = "medium";
       theme = "dark";
+      npmCommand = [
+        "${pkgs.nodejs}/bin/npm"
+        "--prefix"
+        "${config.home.homeDirectory}/.pi/npm-global"
+      ];
       packages = [ "npm:pi-web-access" ];
       compaction = {
         enabled = true;
