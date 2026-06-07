32bbbc4ae9490f7b65f2dfcbf310492275960827
LukeTandjung <lukelucus123@gmail.com>
2026-06-07 18:50:59 +0100

    Fixed bug with kitty config causing recursive autowatching of the entire nix store

--- home_modules/kitty.nix
+++ home_modules/kitty.nix
@@ -1,8 +1,5 @@
 {
-  config,
   pkgs,
-  lib,
-  inputs,
   ...
 }:
 {
@@ -12,6 +9,12 @@
     settings = {
       macos_option_as_alt = "yes";
       enabled_layouts = "horizontal";
+
+      # Disable kitty's automatic config watcher.
+      # Stylix generates kitty theme includes in /nix/store; kitty's watcher can
+      # recursively watch large parts of the store and exhaust inotify watches.
+      # Config is managed by Nix/Home Manager, so manual reloads or new windows are enough.
+      auto_reload_config = -1;
     };
   };
 }
