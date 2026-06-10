289c300bd365fd533b17b32673789eb19eab97af
LukeTandjung <lukelucus123@gmail.com>
2026-06-10 15:43:35 +0100

    Removed electron overlay

--- home_modules/vesktop.nix
+++ home_modules/vesktop.nix
@@ -1,10 +1,4 @@
-{
-  config,
-  pkgs,
-  lib,
-  inputs,
-  ...
-}:
+{ pkgs, ... }:
 {
   programs.vesktop = {
     enable = true;
--- hosts/Lukes-MacBook-Pro/configuration.nix
+++ hosts/Lukes-MacBook-Pro/configuration.nix
@@ -21,26 +21,6 @@
   nixpkgs = {
     hostPlatform = "aarch64-darwin";
     config.allowUnfree = true;
-    overlays = [
-      # Vesktop codesign fix (https://github.com/nixos/nixpkgs/issues/484618)
-      (final: prev: {
-        vesktop = prev.vesktop.overrideAttrs (old: {
-          buildPhase = ''
-            runHook preBuild
-
-            pnpm build
-            pnpm exec electron-builder \
-              --dir \
-              -c.asarUnpack="**/*.node" \
-              -c.electronDist=${if prev.stdenv.hostPlatform.isDarwin then "." else "electron-dist"} \
-              -c.electronVersion=${prev.electron.version} \
-              ${prev.lib.optionalString prev.stdenv.hostPlatform.isDarwin "-c.mac.identity=null"}
-
-            runHook postBuild
-          '';
-        });
-      })
-    ];
   };
 
   fonts.packages = [
