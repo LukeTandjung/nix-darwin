3b3e100f50ba0096dffc34f2a70291d5893de460
LukeTandjung <lukelucus123@gmail.com>
2026-06-28 20:24:10 +0100

    Used newer waydroid package

--- nixos_modules/waydroid.nix
+++ nixos_modules/waydroid.nix
@@ -3,8 +3,7 @@
 {
   virtualisation.waydroid = {
     enable = true;
-    # If Waydroid networking breaks on newer kernels, uncomment this:
-    # package = pkgs.waydroid-nftables;
+    package = pkgs.waydroid-nftables;
   };
 
   environment.systemPackages = with pkgs; [
