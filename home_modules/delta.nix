fc5e7f8e5c861ab4bbfb1f4188ea03eeb806a49e
LukeTandjung <lukelucus123@gmail.com>
2026-09-17 10:51:27 +0100

    feat(delta): enable shared personal instructions

--- home_modules/delta.nix
+++ home_modules/delta.nix
@@ -2,6 +2,7 @@
 
 {
   programs.zed-delta.skills.enable = true;
+  programs.zed-delta.context.enable = true;
 
   home.packages = lib.optionals (builtins.elem pkgs.stdenv.hostPlatform.system [
     "x86_64-linux"
