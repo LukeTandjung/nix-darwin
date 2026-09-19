a1dded4d8971dd1e5a129cc94e16721a153fb41b
LukeTandjung <lukelucus123@gmail.com>
2026-09-19 15:13:51 +0100

    chore: disable GNOME Keyring and Seahorse

--- nixos_modules/desktop.nix
+++ nixos_modules/desktop.nix
@@ -43,10 +43,6 @@
     xwayland.enable = true;
   };
 
-  # Provides Secret Service storage so Claude Desktop/Electron apps can save sign-ins.
-  services.gnome.gnome-keyring.enable = true;
-  programs.seahorse.enable = true;
-
   # Terminal
   environment.sessionVariables.TERMINAL = "kitty";
 
